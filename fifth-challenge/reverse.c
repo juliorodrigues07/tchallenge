c3779c48f4a63503e6582922282061631cf3ea35
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-02-25 15:58:09 -0300

    create: Added function to reverse strings and its header file

---
+++ fifth-challenge/reverse.c
@@ -0,0 +1,25 @@
+#include "reverse.h"
+
+// Reverses the string swapping each character pair from the outside in (towards the middle)
+bool reverse(char *str, unsigned int begin, unsigned int end) {
+
+     /* If the positions analyzed are at the middle or beyond, stops the reversing procedure.
+        At the middle, the swap has no effect, and beyond that,
+        it would start re-reversing the string to its original content. */
+    if (begin >= end)
+        return true;
+
+    // Swaps the 'beginning' and 'ending' characters pair of the string, using an aux variable
+    char swap = str[begin];
+    str[begin] = str[end];
+    str[end] = swap;
+
+    // Increment and decrements indexes to analyze the resultant substrings iteratively
+    begin++;
+    end--;
+
+    // Reverses the string recursively (tail recursion)
+    reverse(str, begin, end);
+
+    // TODO: Fix possibly not returning any value in some control path
+}
---
+++ fifth-challenge/reverse.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool reverse(char *str, unsigned int begin, unsigned int end);
